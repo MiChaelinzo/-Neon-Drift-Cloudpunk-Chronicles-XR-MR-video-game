da74042b181f77f64a97510fbae1ca8005e38f09
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2024-05-07 16:16:14 +0300

    Update tronbox.js

--- tronbox.js
+++ tronbox.js
@@ -1,14 +1,15 @@
 module.exports = {
     networks: {
         development: {
-            from: '',
-            privateKey: '',
-            userFeePercentage: 30,
-            feeLimit: 1e9,
-            originEnergyLimit: 1e7,
-            fullHost: "https://api.shasta.trongrid.io",
-            network_id: "*" // Match any network id
+            from: 'some address',
+            privateKey: 'some private key',
+            consume_user_resource_percent: 30,
+            fee_limit: 100000000,
+            fullNode: "https://api.trongrid.io",
+            solidityNode: "https://api.trongrid.io",
+            eventServer: "optional",
+            network_id: "*"
         },
-        production: {}
+        // Add other network configurations as needed
     }
 };
