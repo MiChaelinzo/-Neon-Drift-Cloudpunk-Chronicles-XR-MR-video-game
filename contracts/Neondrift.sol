5efcc400e95fa8299d32bd7bd06cc29f8ea7b970
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2024-05-07 16:10:49 +0300

    Create Neondrift.sol

---
+++ contracts/Neondrift.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.23 <0.6.0;
+
+contract Test {
+    function f() public pure returns (string memory) {
+        return "method f()";
+    }
+
+    function g() public pure returns (string memory) {
+        return "method g()";
+    }
+}
